a9c8551f0540e0c571ea99f2cd5a6ed4478e4202
Tom Rhodan <tomrhodan@gmail.com>
2026-03-24 08:44:36 +0000

    chore(release): bump version to v0.4.4 and update app screenshots

--- src-tauri/src/prefix_tools.rs
+++ src-tauri/src/prefix_tools.rs
@@ -304,7 +304,7 @@ pub async fn install_powershell(
 
     let client = reqwest::Client
         ::builder()
-        .user_agent("penguin-citizen/0.4.2")
+        .user_agent("penguin-citizen/0.4.4")
         .connect_timeout(std::time::Duration::from_secs(10))
         .timeout(std::time::Duration::from_secs(30))
         .build()
